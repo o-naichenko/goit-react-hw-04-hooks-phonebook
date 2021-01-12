5d6d46d930f0c6ee09783ffeffabb25fd306216a
o-naichenko <o.naichenko@gmail.com>
2021-01-12 15:12:11 +0200

    Додає імпорт React в компоненти

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { v4 as uuidv4 } from "uuid";
 import s from "./ContactForm.module.css";
